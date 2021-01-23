d56354f6cbeb0a07d0a44727874d06a1570b1827
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-01-23 04:00:09 -0800

    Update config file

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,6 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'https://mnn59.github.io',
-  pathPrefix: '/mnn59.github.io',
+  siteUrl: 'https://mnn59.github.io',
   plugins: []
 }
